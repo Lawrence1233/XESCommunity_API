a672280c47eaebb88af198f8c290c35575aee481
Lawrence1233 <102820703+Lawrence1233@users.noreply.github.com>
2022-06-14 19:38:16 +0800

    Update xes_api.py
    full

--- xes_api.py
+++ xes_api.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-def save(name,xml,cookie,headers={},type_='webpy'):
+def save(Name :str,xml,Cookie :str,headers={},type_='webpy')->object:
     """
     保存作品
     :param name: 作品名
@@ -12,27 +12,26 @@ def save(name,xml,cookie,headers={},type_='webpy'):
     :return: 返回请求变量
     """
     if headers=={}:
-        headers={'Cookie':cookie,
+        headers={'Cookie':Cookie,
                  'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33"
                  }
 
     base = "https://code.xueersi.com/api/compilers/save"
-    data = {"name": name, "xml": xml, "type": type_, "lang": type_, "id": '',
+    data = {"name": Name, "xml": xml, "type": type_, "lang": type_, "id": '',
             "original_id": 3, "version": "webpy", "args": [], "planid": 'null', "problemid": '', "projectid": 3,
             "code_complete": 1, "removed": 0, "user_id": 8510061,
             "assets": {"assets": [], "cdns": ["https://livefile.xesimg.com"], "hide_filelist": False}}
     res=requests.post(base,data=data,headers=headers)
     return res
 
-def push(res,name='NONE'):
+def push(res,name='NONE')->object:
     """
     发布作品
     :param res: 请求变量
     :param name: 作品名
     :return: 请求变量
     """
-    global cookie
-    global headers
+
     res=eval(res.replace('true',"True").replace('false',"False"))
     ID=res['data']['id']
     print(ID)
@@ -43,7 +42,7 @@ def push(res,name='NONE'):
     res=requests.post(base%ID,headers=headers,data=data)
     return res
 
-def report(cookie,reason,reason_detail,complaint_reason_url,id_):
+def report(Cookie :str,Reason :str,reason_detail :int,complaint_reason_url :str,id_ :int)->object:
     """
     举报作品
     :param cookie:COOKIE
@@ -54,12 +53,12 @@ def report(cookie,reason,reason_detail,complaint_reason_url,id_):
     :return: 请求变量
     """
     base = "https://code.xueersi.com/api/projects/submit_complaint"
-    data = {"reason": reason, "reason_detail": reason_detail, "complaint_reason_images": [],
+    data = {"reason": Reason, "reason_detail": reason_detail, "complaint_reason_images": [],
             "complaint_reason_url": complaint_reason_url, "id": id_}
 
     res = requests.post(base, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33',
-        'Cookie': cookie},
+        'Cookie': Cookie},
                         data=data)
     return res
     
@@ -104,19 +103,73 @@ def Send(Content : str,Type : int,Cookie: str,Id_ : int,UserAgent='',headers={})
                         data={"appid": 1001108, 'topic_id': Type + str(Id_), 'target_id': '0', 'content': Content})
     return res
 
-def check(ID):
+def get_info(ID :int)->dict:
+    """
+    获取作品信息
+    :param ID: 作品ID
+    :return: dict
+    """
+    base="https://code.xueersi.com/api/compilers/v2/%s?id=%s"%(ID,ID)
+    res=requests.get(base,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33'})
+    f=eval(res.text.replace('null','\'null\'').replace('true','\'true\'').replace('false','\'false\''))
+    return f
+
+
+def check(ID) -> bool:
     """
     检查作品是否存在
     :param ID:作品ID
     :return: 作品是否存在（bool）是（True）否（False）
     """
-    base="https://code.xueersi.com/api/compilers/v2/%s?id=%s"%(ID,ID)
-    res=requests.get(base,headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.33'})
-    f=eval(res.text.replace('null','\'null\'').replace('true','\'true\'').replace('false','\'false\''))
+    get_info(ID)
     if 'status_code' in f:
-        return False,0
+        return False
     else:
         if f['data']['published_at'] != '0000-00-00 00:00:00':
             return True
         else:
             return False
+
+
+def parse(Dict :dict)->dict:
+    """
+    解析作品常用信息
+
+    name: 作品名
+    type: 类型（homework或normal等）
+    description: 通常为简介，也有可能显示在xml项
+    xml: 可能为简介
+    user_id: 作者ID
+    thumbnail: 封面链接
+    modified_at: 最后一次修改日期
+    likes: 点赞
+    views: 浏览、观看数
+    comments: 评论数
+    deleted_at: 删除时间（通常为空）
+    created_at: 创建时间
+    updated_at: 提交时间
+
+    :param Dict: get_info的返回值（或其他可分析的字典）
+    :return:
+
+
+    """
+    if 'status_code' in Dict:#失败
+        return {'stat':'0','msg':Dict['msg']}
+
+    _output={
+        "name":Dict['data']['name'],
+        'type':Dict['data']['type'],
+        'description':Dict['data']['description'],
+        'xml': Dict['data']['xml'],
+        'user_id': Dict['data']['user_id'],
+        'thumbnail':Dict['data']['thumbnail'],
+        'modified_at':Dict['data']['modified_at'],
+        'likes':Dict['data']['likes'],
+        'views':Dict['data']['views'],
+        'comments':Dict['data']['comments'],
+        'deleted_at':Dict['data']['deleted_at'],
+        'created_at':Dict['data']['created_at'],
+        'updated_at':Dict['data']['updated_at']
+    }
+    return _output
